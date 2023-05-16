ca00fead1a64c209da684d138df9c9982b04d6e4
eliott23 <62487535+eliott23@users.noreply.github.com>
2023-05-16 04:41:37 +0100

    w00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -9,27 +9,36 @@
 #include <sstream>
 #include <iomanip>
 
-bool isValidDate(const std::tm& date) {
+void    isValidDate(const std::tm& date) {
 
     // Check if the month has a valid number of days
     static const int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int daysInCurrentMonth = daysInMonth[date.tm_mon];
 
-    // Account for leap years (February has 29 days)
-    if (date.tm_mon == 1 && ((date.tm_year % 4 == 0 && date.tm_year % 100 != 0) || date.tm_year % 400 == 0))
-    {
-        if (date.tm_mday > 29 )
-    }
 
     if (date.tm_mday > daysInCurrentMonth)
     {
-        std::cout << "Error: unvalid date" << std::endl;
-        exit(0);
+    // Account for leap years (February has 29 days)
+        if (date.tm_mon == 1 && ((date.tm_year % 4 == 0 && date.tm_year % 100 != 0) || date.tm_year % 400 == 0))
+        {
+            if (date.tm_mday > 29)
+            {
+                std::cout << "Error: unvalid date" << std::endl;
+                exit(0);
+            }
+        }
+        else
+        {
+            std::cout << "Error: unvalid date" << std::endl;
+            exit(0);
+        }
     }
 }
 
 int main(int ac, char **av)
 {
+    char    c;
+    float    j;
     std::exception e;
     std::string line;
     std::vector<std::pair<int, int> > k;
@@ -39,7 +48,6 @@ int main(int ac, char **av)
         exit(0);
     }
     std::ifstream h(av[1]);
-
     if (!h.is_open())
     {
         std::cout << "Error: could not open file" << std::endl;
@@ -48,7 +56,6 @@ int main(int ac, char **av)
     getline(h, line);
     line.clear();
     getline(h, line);
-    // std::cout << line << std::endl;
     std::istringstream ss(line);
     std::tm tm  = {};
     ss >> std::get_time(&tm, "%Y-%m-%d");
@@ -57,9 +64,14 @@ int main(int ac, char **av)
     else
     {
         tm.tm_year += 1900;
-        valid_date(tm);
+        isValidDate(tm);
         std::cout << tm.tm_year << "-"\
         << tm.tm_mon << "-" << tm.tm_mday << std::endl;
+        if (ss >> c) 
+            std::cout << "hh" << std::endl;
+        if (c != ',')
+            std::cout << "Error unvalid format ==> " << line  << std::endl;
+        if (ss >> j)
+            std::cout << j << std::endl;
     }
-    (void)tm;
 }
