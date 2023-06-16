4850298b931bdda5dec67db65a23517b8bf8ea57
eliott23 <62487535+eliott23@users.noreply.github.com>
2023-06-16 18:59:00 +0100

    s

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,8 +2,32 @@
 #include <istream>
 #include <sstream>
 #include <string>
+#include <array>
+#include <list>
+#include <deque>
+
+void    rexit()
+{
+    std::cout << "Error" << std::endl;
+    exit(0);
+}
 
 int main(int ac, char *av[])
 {
-
+    if (ac < 2)
+        rexit();
+    std::deque<int> deq;
+    unsigned int temp;
+    int i = 0;
+    while (av[++i])
+    {
+        std::istringstream ss(av[i]);
+        if (ss >> temp && !ss.rdbuf()->in_avail())
+        {
+            deq.push_back(temp);
+            std::cout << temp << std::endl;
+        }
+        else
+            rexit();
+    }
 }
